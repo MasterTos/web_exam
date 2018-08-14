62505107ea3c7a5a180e4a39fb3e82bb6d717e7c
MasterTos <MasterTos@yahoo.com>
2018-08-14 18:20:56 +0700

    add production

--- sciproject/run.sh
+++ sciproject/run.sh
@@ -1,3 +1,12 @@
-python manage.py collectstatic
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python manage.py collectstatic --noinput
+python manage.py migrate --noinput
+#python manage.py runserver 0.0.0.0:8000
+
+exec  gunicorn --bind=0.0.0.0:80 sciproject.wsgi \
+        --workers=5\
+        --log-level=info \
+        --log-file=-\
+        --access-logfile=-\
+        --error-logfile=-\
+        --timeout 30000\
+        --reload
